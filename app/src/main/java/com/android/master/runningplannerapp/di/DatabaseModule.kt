8e6858332298c578e886790f5734bca2254d78da
dev-baik <wlsdn9297@naver.com>
2024-10-26 18:02:13 +0900

    feat: DatabaseModule 구현

---
+++ app/src/main/java/com/android/master/runningplannerapp/di/DatabaseModule.kt
@@ -0,0 +1,26 @@
+package com.android.master.runningplannerapp.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            AppDatabase.DB_NAME
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+}
