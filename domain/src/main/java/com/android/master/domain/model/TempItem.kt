f39988de65eb0f6effde810aab16f982ed1cefcb
dev-baik <wlsdn9297@naver.com>
2024-10-30 12:18:17 +0900

    feat: Parcelable 추가

--- domain/src/main/java/com/android/master/domain/model/TempItem.kt
+++ domain/src/main/java/com/android/master/domain/model/TempItem.kt
@@ -1,5 +1,9 @@
 package com.android.master.domain.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class TempItem(
     val data: String
-)
+): Parcelable
