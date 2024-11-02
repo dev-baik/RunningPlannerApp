27280d4f4fc8d94dcb7f6e21ce181f53347fc8de
dev-baik <wlsdn9297@naver.com>
2024-11-02 12:53:31 +0900

    feat: AccountRepositoryImpl 구현

--- app/src/main/java/com/android/master/runningplannerapp/di/DataSourceModule.kt
+++ app/src/main/java/com/android/master/runningplannerapp/di/DataSourceModule.kt
@@ -2,6 +2,7 @@ package com.android.master.runningplannerapp.di
 
 import com.android.master.data.datasource.mock.MockDataSource
 import com.android.master.data.datasource.mock.MockDataSourceImpl
+import com.android.master.data.repository.AccountRepositoryImpl
 import com.android.master.domain.repository.AccountRepository
 import dagger.Binds
 import dagger.Module
