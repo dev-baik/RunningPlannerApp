003f42956f4390931c1511bded7ea28e7e83b229
dev-baik <wlsdn9297@naver.com>
2025-03-27 22:58:28 +0900

    feat: hilt 설정 추가

---
+++ app/src/main/java/com/android/master/runningplannerapp/RPApplication.kt
@@ -0,0 +1,8 @@
+package com.android.master.runningplannerapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RPApplication : Application() {
+}
