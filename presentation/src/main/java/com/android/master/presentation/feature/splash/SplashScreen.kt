b940f46c15d64b9d04ad6e2710ffd0ae2835e7cf
dev-baik <wlsdn9297@naver.com>
2025-04-03 11:47:59 +0900

    feat: SplashScreen Image 추가

--- presentation/src/main/java/com/android/master/presentation/feature/splash/SplashScreen.kt
+++ presentation/src/main/java/com/android/master/presentation/feature/splash/SplashScreen.kt
@@ -4,9 +4,11 @@ import android.os.Build
 import android.view.View
 import android.view.Window
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
@@ -70,11 +73,17 @@ fun SplashScreen() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Spacer(modifier = Modifier.weight(226f / (226f + 284f)))
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = stringResource(id = R.string.splash_logo_description)
+        )
         Text(
             text = stringResource(R.string.splash_title),
             color = RPAppTheme.colors.white,
             style = RPAppTheme.typography.titleSemi24
         )
+        Spacer(modifier = Modifier.weight(284f / (226f + 284f)))
     }
 }
 
