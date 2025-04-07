136404e57b39deba07f0bd52a65a7b8922a0d340
dev-baik <wlsdn9297@naver.com>
2025-04-07 11:25:00 +0900

    chore: java version update from 11 to 21

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     id("java-library")
+    id("org.jetbrains.kotlin.jvm")
     id("kotlin-kapt")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 dependencies {
